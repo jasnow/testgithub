49ed7de29fda3ee9ecbbc1ad50d1729f3a8696b3
Al Snow <jasnow@hotmail.com>
2017-11-27 15:10:07 -0500

    Upgraded Rails to 5.2.0.beta1 + arel gem

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -7,7 +7,7 @@ describe StaticPagesController, :type => :controller do
     end
 
     it "responds successfully with an HTTP 200 status code" do
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
 
