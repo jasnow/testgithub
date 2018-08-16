7893448a0b2afbebb586d75a9f68993d475cd5c9
Al Snow <jasnow@hotmail.com>
2018-08-16 08:34:38 -0400

    Fixed factory-bot dep warning

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,14 +3,14 @@ FactoryBot.define do
 
   factory :user do
     email 
-    password "1234567890"
+    password { "1234567890" }
   end
 
   factory :admin, class: User do
     email
-    password "213mdrsat"
-    first_name "Peter"
-    last_name "Example"
-    admin false
+    password { "213mdrsat" }
+    first_name { "Peter" }
+    last_name { "Example" }
+    admin { false }
   end
 end
